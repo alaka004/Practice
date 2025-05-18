dfd37722640466eae67989c994ce3d76c56b4987
alaka004 <alaka9704@gmail.com>
2025-05-18 23:19:35 +0530

    conflict resolved

--- volume.java
+++ volume.java
@@ -2,12 +2,12 @@ class volume
 {
 	public static void main(string[] args)
 	{
-<<<<<<< HEAD
+
 		int n=10,v;
                 //volume of cube
-=======
+
 		int n=12,v;
->>>>>>> 9f5675121ac9c201ec3165ca26305e9ab0de85c2
+
 		v=n*n*n;
 		System.out.println("Volume =" +v);
 	}
